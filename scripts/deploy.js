5574a9ae848e13465262313b6e866c18784aa09d
reef <reef@nepla.io>
2024-07-15 11:24:40 +0900

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 const hre = require('hardhat');
 
 async function main() {
-  const Nft = await hre.ethers.getContractFactory('Nft');
+  const NeplaNft = await hre.ethers.getContractFactory('NeplaNft');
+  const neplaNft = await NeplaNft.deploy(process.env.NFT_NAME, process.env.NFT_SYMBOL, process.env.OWNER_ADDRESS);
+
+  await neplaNft.deployed();
+
+  console.log(`NEPLA NFT contract has been deployed : ${neplaNft.address}`);
 }
 
 main()
